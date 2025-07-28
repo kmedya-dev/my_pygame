8c653337e70efd704b0bd6719678c0ef4f25c2c9
kmedya-dev <koustavamedya.2000@gmail.com>
2025-07-29 01:20:05 +0530

    Update build.sh to use relative path for main.py

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ echo "Running build script..."
 python3 -m pip install --upgrade pip
 # If your scripts had external dependencies, you would list them here, e.g.:
 # pip install -r requirements.txt
-python3 /root/DEVICE_BACKUP/main.py
+python3 ./main.py
